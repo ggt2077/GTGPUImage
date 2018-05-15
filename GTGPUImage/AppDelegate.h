b7446a49b3ea8514088c2e1169d6c5f1fafb1580
Tony <guoguangtao01@zhuanzhuan.com>
2018-05-15 10:31:36 +0800

    Initial Commit

---
+++ GTGPUImage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GTGPUImage
+//
+//  Created by Tony on 2018/5/15.
+//  Copyright © 2018年 58. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GTGPUImage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GTGPUImage
+//
+//  Created by Tony on 2018/5/15.
+//  Copyright © 2018年 58. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
