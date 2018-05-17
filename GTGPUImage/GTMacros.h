9a7696f617e93fac05c357bc4a7ca84994cfa8b4
Tony <guoguangtao01@zhuanzhuan.com>
2018-05-17 20:31:17 +0800

    更新拍摄照片

---
+++ GTGPUImage/GTMacros.h
@@ -0,0 +1,44 @@
+//
+//  GTMacro.h
+//  GTGPUImage
+//
+//  Created by Tony on 2018/5/17.
+//  Copyright © 2018年 58. All rights reserved.
+//
+
+#ifndef GTMacro_h
+#define GTMacro_h
+
+#ifndef weakify
+    #if DEBUG
+        #if __has_feature(objc_arc)
+            #define weakify(object) autoreleasepool{} __weak __typeof__(object) weak##_##object = object;
+        #else
+            #define weakify(object) autoreleasepool{} __block __typeof__(object) block##_##object = object;
+        #endif
+    #else
+        #if __has_feature(objc_arc)
+            #define weakify(object) try{} @finally{} {} __weak __typeof__(object) weak##_##object = object;
+        #else
+            #define weakify(object) try{} @finally{} {} __block __typeof__(object) block##_##object = object;
+        #endif
+    #endif
+#endif
+
+#ifndef strongify
+    #if DEBUG
+        #if __has_feature(objc_arc)
+            #define strongify(object) autoreleasepool{} __typeof__(object) object = weak##_##object;
+        #else
+            #define strongify(object) autoreleasepool{} __typeof__(object) object = block##_##object;
+        #endif
+    #else
+        #if __has_feature(objc_arc)
+            #define strongify(object) try{} @finally{} __typeof__(object) object = weak##_##object;
+        #else
+            #define strongify(object) try{} @finally{} __typeof__(object) object = block##_##object;
+        #endif
+    #endif
+#endif
+
+#endif /* GTMacro_h */
--- GTGPUImage/PhotoViewController.m
+++ GTGPUImage/PhotoViewController.m
@@ -8,12 +8,15 @@
 
 #import "PhotoViewController.h"
 #import <GPUImage/GPUImage.h>
+#import "GTMacros.h"
 
 @interface PhotoViewController ()
 
 @property (strong, nonatomic) GPUImageView *imageView;
 @property (nonatomic, strong) GPUImageStillCamera *camera;
 @property (nonatomic, strong) GPUImageFilter *filter;
+@property (nonatomic, strong) UIButton *takePhotoButton;
+@property (nonatomic, strong) UIImageView *photoView;
 
 @end
 
@@ -23,6 +26,13 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     [self.view addSubview:self.imageView];
+    
+    self.takePhotoButton.frame = CGRectMake(0, 0, 100, 50);
+    self.takePhotoButton.center = self.view.center;
+    [self.view addSubview:self.takePhotoButton];
+    
+    self.photoView.frame = CGRectMake(0, self.view.bounds.size.height - 100, 100, 100);
+    [self.view addSubview:self.photoView];
     
     // 设置背景色
     [_imageView setBackgroundColorRed:1.0 green:1.0 blue:1.0 alpha:1.0];
@@ -38,11 +48,50 @@
     [_camera startCameraCapture];
 }
 
+- (void)pictureButtonTapped:(UIButton *)sender
+{
+    if ([_camera.captureSession isRunning]) {
+        [_camera capturePhotoAsImageProcessedUpToFilter:_filter withCompletionHandler:^(UIImage *processedImage, NSError *error) {
+            [_camera stopCameraCapture];
+            if (!error) {
+                [_photoView setImage:processedImage];
+            }
+        }];
+    } else {
+        [_camera startCameraCapture];
+    }
+}
+
+#pragma mark -- Getter --
+
 - (GPUImageView *)imageView {
     if (!_imageView) {
         _imageView = [[GPUImageView alloc] initWithFrame:self.view.bounds];
     }
     return _imageView;
+}
+
+- (UIButton *)takePhotoButton {
+    if (!_takePhotoButton) {
+        _takePhotoButton = [UIButton buttonWithType:UIButtonTypeCustom];
+        [_takePhotoButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+        _takePhotoButton.backgroundColor = [[UIColor blackColor] colorWithAlphaComponent:0.6];
+        _takePhotoButton.titleLabel.font = [UIFont systemFontOfSize:30];
+        _takePhotoButton.layer.borderColor = [UIColor whiteColor].CGColor;
+        _takePhotoButton.layer.borderWidth = 0.5;
+        _takePhotoButton.layer.cornerRadius = 2.0;
+        _takePhotoButton.layer.masksToBounds= YES;
+        [_takePhotoButton setTitle:@"拍摄" forState:UIControlStateNormal];
+        [_takePhotoButton addTarget:self action:@selector(pictureButtonTapped:) forControlEvents:UIControlEventTouchUpInside];
+    }
+    return _takePhotoButton;
+}
+
+- (UIImageView *)photoView {
+    if (!_photoView) {
+        _photoView = [[UIImageView alloc] init];
+    }
+    return _photoView;
 }
 
 @end
